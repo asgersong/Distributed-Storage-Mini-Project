3aba84b7077546f5614cca2dcc66ff2b08f4329b
Andreas691667 <andreaskaag@hotmail.com>
2024-12-22 09:39:23 +0100

    Implement replication group initiation and buddy selection strategy in node_selection.py

--- src/lead_node/node_selection.py
+++ src/lead_node/node_selection.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 RANDOM_SELECTION = "random"
 MIN_COPY_SETS_SELECTION = "min_copy_sets"
@@ -16,6 +17,21 @@ class NodeSelectionStrategy:
     def choose_nodes(self):
         """Select <no_replicas> nodes for each fragment"""
         raise NotImplementedError("This method must be implemented by the subclass")
+
+    def initiate_replication_groups(self, no_groups, group_size):
+        """Initiate the replication group for all nodes"""
+        replication_groups = []
+        available_nodes = list(range(1, self.no_nodes + 1))
+        random.shuffle(available_nodes)
+        for _ in range(no_groups):
+            group = random.sample(available_nodes, group_size)
+            replication_groups.append(group)
+            for node in group:
+                available_nodes.remove(node)
+
+        # remove unused nodes from available nodes
+        available_nodes = [node for group in replication_groups for node in group]
+        return replication_groups, available_nodes
 
 
 class RandomSelection(NodeSelectionStrategy):
@@ -58,30 +74,35 @@ class MinCopySetsSelection(NodeSelectionStrategy):
         """Initiate the copy sets for all nodes"""
 
         no_copysets = self.no_nodes // self.no_replicas
-
-        copy_sets = []
-        available_nodes = list(
-            range(1, self.no_nodes + 1)
-        )  # List of node IDs (1 to no_nodes)
-
-        random.shuffle(available_nodes)
-
-        for _ in range(no_copysets):
-            copy_set = random.sample(available_nodes, self.no_replicas)
-            copy_sets.append(copy_set)
-
-            # Remove the used nodes
-            for node in copy_set:
-                available_nodes.remove(node)
-
-        self.copy_sets = copy_sets
-
-        # available nodes are those that are in the copy_sets
-        self.available_nodes = [node for copy_set in copy_sets for node in copy_set]
+        self.copy_sets, self.available_nodes = self.initiate_replication_groups(
+            no_copysets, self.no_replicas
+        )
 
 
 class BuddySelection(NodeSelectionStrategy):
     """Selects K nodes using the buddy system"""
 
+    def __init__(self, no_nodes, no_fragments, no_replicas):
+        super().__init__(no_nodes, no_fragments, no_replicas)
+        self.buddies = None
+        self.available_nodes = None
+        self.__initiate_buddies()
+
     def choose_nodes(self):
-        pass
+        # choose no_fragments buddy groups at random
+        buddy_groups = random.choices(range(len(self.buddies)), k=self.no_fragments)
+
+        # within each buddy group, choose no_replicas nodes at random for each fragment
+        chosen = []
+        for buddy_group in buddy_groups:
+            selected_nodes = random.sample(self.buddies[buddy_group], self.no_replicas)
+            chosen.append(selected_nodes)
+        return list(map(list, zip(*chosen)))
+
+    def __initiate_buddies(self):
+        """Initiate the replication group for all nodes"""
+        no_groups = math.floor(math.sqrt(self.no_nodes / self.no_replicas))
+        self.buddies, self.available_nodes = self.initiate_replication_groups(
+            no_groups, self.no_nodes // no_groups
+        )
+        print("Buddies:", self.buddies)
