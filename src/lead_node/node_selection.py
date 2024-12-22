484dca61e4a347fd4b505ade594e58c8fe58850d
Andreas691667 <andreaskaag@hotmail.com>
2024-12-22 08:56:09 +0100

    improved node selection in mincopysets

--- src/lead_node/node_selection.py
+++ src/lead_node/node_selection.py
@@ -6,7 +6,7 @@ BUDDY_SELECTION = "buddy"
 
 
 class NodeSelectionStrategy:
-    """ "Parent class for node selection strategies"""
+    """Parent class for node selection strategies"""
 
     def __init__(self, no_nodes, no_fragments, no_replicas):
         self.no_nodes = no_nodes
@@ -20,6 +20,7 @@ class NodeSelectionStrategy:
 
 class RandomSelection(NodeSelectionStrategy):
     """Selects nodes randomly"""
+
     def choose_nodes(self):
         chosen = []
         for _ in range(self.no_replicas):
@@ -29,22 +30,29 @@ class RandomSelection(NodeSelectionStrategy):
             chosen.append(selected_nodes)
         return chosen
 
+
 class MinCopySetsSelection(NodeSelectionStrategy):
     """Selects nodes using the minimum copy sets algorithm"""
 
     def __init__(self, no_nodes, no_fragments, no_replicas):
         super().__init__(no_nodes, no_fragments, no_replicas)
         self.copy_sets = None
+        self.available_nodes = None
         self.__initiate_copy_sets()
 
     def choose_nodes(self):
-        chosen = []
-        for _ in range(self.no_fragments):
-            selected_nodes = random.choice(self.copy_sets)
-            chosen.append(selected_nodes)
-        
-        # return transpose of the chosen nodes
-        return [list(x) for x in zip(*chosen)]
+        # choose no_fragments nodes at random
+        primary_nodes = random.choices(self.available_nodes, k=self.no_fragments)
+
+        # find the copy sets that contain the primary nodes
+        copy_sets = []
+        for primary_node in primary_nodes:
+            for copy_set in self.copy_sets:
+                if primary_node in copy_set:
+                    copy_sets.append(copy_set)
+                    break
+        # return transpose of copy_sets
+        return list(map(list, zip(*copy_sets)))
 
     def __initiate_copy_sets(self):
         """Initiate the copy sets for all nodes"""
@@ -53,7 +61,7 @@ class MinCopySetsSelection(NodeSelectionStrategy):
 
         copy_sets = []
         available_nodes = list(
-            range(1, self.no_nodes+1)
+            range(1, self.no_nodes + 1)
         )  # List of node IDs (1 to no_nodes)
 
         random.shuffle(available_nodes)
@@ -67,6 +75,10 @@ class MinCopySetsSelection(NodeSelectionStrategy):
                 available_nodes.remove(node)
 
         self.copy_sets = copy_sets
+
+        # available nodes are those that are in the copy_sets
+        self.available_nodes = [node for copy_set in copy_sets for node in copy_set]
+
 
 class BuddySelection(NodeSelectionStrategy):
     """Selects K nodes using the buddy system"""
